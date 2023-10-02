654cde3d2382b483ffab537d15f94d97add1507c
“lincgcg <chunganglin@gmail.com>
2023-10-02 21:50:38 +0800

    debug

--- model.py
+++ model.py
@@ -134,8 +134,8 @@ class FSNet(object):
                                                      global_step=self.global_step)
 
     def _embedding(self, emb_dim, vac_num, inputs, scope='embedding'):
-        with tf.variable_scope(scope):
-            embedding = tf.get_variable('embedding', dtype=tf.float32, shape=[vac_num, emb_dim])
+        with tf.compat.v1.variable_scope(scope):
+            embedding = tf.compat.v1.get_variable('embedding', dtype=tf.float32, shape=[vac_num, emb_dim])
             seq = tf.nn.embedding_lookup(embedding, inputs)
         return seq
 
@@ -176,8 +176,11 @@ class FSNet(object):
 
     def _reconstruct(self, inputs, vac_num, label, mask, scope='rec'):
         with tf.variable_scope(scope):
-            logits = tf.keras.layers.Dense(inputs, self.config.hidden, activation=tf.nn.selu)
-            logits = tf.keras.layers.Dense(logits, vac_num)
+            # logits = tf.keras.layers.Dense(inputs, self.config.hidden, activation=tf.nn.selu)
+            # logits = tf.keras.layers.Dense(logits, vac_num)
+            logits = tf.keras.layers.Dense(self.config.hidden, activation=tf.nn.selu)(inputs)
+            logits = tf.keras.layers.Dense(vac_num)(logits)
+
             logits = tf.reshape(logits, [-1, vac_num])
             loss_all = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=tf.reshape(label, [-1]))
             mask = tf.cast(tf.reshape(mask, [-1]), dtype=tf.float32)
