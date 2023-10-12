ff6d02b9b359b7b69b0fc15fc7aa09faead6a002
“lincgcg <chunganglin@gmail.com>
2023-10-12 13:36:53 +0800

    debug

---
+++ test_DCS1__FT_01to20.sh
@@ -0,0 +1,140 @@
+
+
+# 01
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/01/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 02
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/02/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 03
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/03/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 04
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/04/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 05
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/05/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 06
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/06/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 07
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/07/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 08
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/08/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 09
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/09/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 10
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/10/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 11
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/11/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 12
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/12/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 13
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/13/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 14
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/14/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 15
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/15/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 16
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/16/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 17
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/17/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 18
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/18/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 19
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/19/result"  --class_num 17  --eval_batch -1
+
+
+
+
+# 20
+
+CUDA_VISIBLE_DEVICES=1 python main.py --mode test --train_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/record/train.json" --test_json "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/record/test.json" --train_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/record/train.meta" --test_meta "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/record/test.meta" --log_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/log" --model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/log" --data_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/filter" --test_model_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/record/log" --pred_dir "/root/FS-Net-file/ISCXVPN/17/dataset/DCS1/20/result"  --class_num 17  --eval_batch -1
+
+
